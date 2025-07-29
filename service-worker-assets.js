80719bb2e5f9c8ea896180176e65fa6be220e64a
Luisito Salvarezza <114780186+DRRLuisito@users.noreply.github.com>
2025-07-29 10:28:08 -0300

    Add files via upload

---
+++ service-worker-assets.js
@@ -0,0 +1,11 @@
+﻿self.assetsManifest = {
+    assets: [
+        { "url": "index.html"},
+        { "url": "manifest.json"},
+        { "url": "icon-192.png"},
+        { "url": "icon-512.png"},
+        { "url": "_framework/blazor.webassembly.js"},
+        { "url": "_framework/dotnet.wasm"}
+    ],
+    version: "1.0.0"
+};
---
+++ service-worker.js
@@ -0,0 +1,43 @@
+﻿importScripts('./service-worker-assets.js');
+
+const CACHE_NAME = 'AlmaWEB-cache-v1';
+
+const OFFLINE_ASSETS = self.assetsManifest.assets.map(asset => asset.url);
+
+
+// Instalar y cachear los assets
+self.addEventListener('install', event => {
+    console.log('Service Worker: Instalando...');
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(OFFLINE_ASSETS);
+        })
+    );
+    self.skipWaiting();
+});
+
+// Activar y limpiar cachés viejas
+self.addEventListener('activate', event => {
+    console.log('Service Worker: Activado');
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    return self.clients.claim();
+});
+
+// Interceptar peticiones y responder desde cache o red
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
